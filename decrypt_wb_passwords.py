9f3f1241b12c41745bcda159cb4e98f1059f6afe
Chris Belson <beaming@msn.com>
2020-03-10 21:13:08 -0400

    Created simple program to decrypt MySQL Workbench passwords

---
+++ decrypt_wb_passwords.py
@@ -0,0 +1,26 @@
+import os
+import win32crypt
+
+## get %appdata% dir ##
+appdata = os.getenv('appdata')
+
+## get wb user data ##
+try:
+    with open(appdata + '/MySQL/Workbench/workbench_user_data.dat', 'rb') as d:
+        data = d.read()
+except FileNotFoundError:
+    print("File does not exist or in use!\n")
+    raise 
+else:
+    raise
+
+## get creds ##
+bad_chars = ['\\x02', '\\x03', '\\n', '\\x00']
+creds = win32crypt.CryptUnprotectData(data)
+for char in bad_chars:
+    creds = str(creds).split(char)
+
+## display creds ##
+print("Your creds are:")
+for cred in creds:
+    print(cred.replace('\\', ''))
